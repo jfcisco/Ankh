319d90e2d5486e770538303e1146b730050e3cc8
Jasper Francisco <jasper.francisco@gmail.com>
2020-07-04 13:22:02 +0800

    Add function to select anime to be played

--- src/Components/AnimeList.jsx
+++ src/Components/AnimeList.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './AnimeList.css';
 
 export default function(props) {
+  function handleAnimeSelection(animeId) {
+    props.setCurrentAnime(animeId);
+  }
+
     const animeList = props.animes.map((anime) => 
     <li 
       key={anime.id}
       className="list-group-item anime-list-item">
         <a
           href="#"
+          data-anime-id={anime.id}
+          onClick={() => handleAnimeSelection(anime.id)}
           >
           {anime.name}
         </a>
